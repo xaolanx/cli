4557501635bdd08b35d1daf74f66500a24df589d
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-28 17:47:34 +1100

    install: fix shell dependency

--- install/shell.fish
+++ install/shell.fish
@@ -2,7 +2,7 @@
 
 . (dirname (status filename))/util.fish
 
-install-deps git dart-sass libastal-meta npm curl libnotify ttf-material-symbols-variable-git ttf-jetbrains-mono-nerd ttf-rubik-vf pacman-contrib
+install-deps git dart-sass libastal-gjs-git libastal-meta npm curl libnotify ttf-material-symbols-variable-git ttf-jetbrains-mono-nerd ttf-rubik-vf pacman-contrib
 install-optional-deps 'uwsm (for systems using uwsm)' 'yay (AUR package management)' 'fd (launcher file search)' 'wl-clipboard (clipboard support)' 'foot (opening stuff in terminal)'
 
 set -l shell $CONFIG/shell
