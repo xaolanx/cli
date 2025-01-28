8c9afe2d5eca7a1494f39ca09a88f8534264b843
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-28 18:17:11 +1100

    install: scripts install completions

--- install/scripts.fish
+++ install/scripts.fish
@@ -7,11 +7,16 @@ install-optional-deps 'vesktop-bin (discord client)' 'btop (system monitor)' 'wf
 
 set -l dist $CONFIG/scripts
 
+# Clone repo
 confirm-overwrite $dist
-
 git clone 'https://github.com/caelestia-dots/scripts.git' $dist
 
+# Install to path
 mkdir -p ~/.local/bin
 ln -s $dist/main.fish ~/.local/bin/caelestia
+
+# Install completions
+mkdir -p $CONFIG/../fish/completions
+cp $dist/completions/caelestia.fish $CONFIG/../fish/completions/caelestia.fish
 
 log 'Done.'
