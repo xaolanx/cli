6f00e6572756810a7bc0a911848fbdd51ff4b8d5
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-29 15:52:57 +1100

    install: hypr install uwsm envs
    Also ydotool optional

--- install/hypr.fish
+++ install/hypr.fish
@@ -3,19 +3,25 @@
 . (dirname (status filename))/util.fish
 
 install-deps git uwsm hyprland-git hyprpaper-git hyprlock-git hypridle-git polkit-gnome gnome-keyring wl-clipboard wireplumber
-install-optional-deps 'gammastep (night light)' 'wlogout (secondary session menu)' 'grimblast-git (screenshot freeze)' 'hyprpicker-git (colour picker)' 'foot (terminal emulator)' 'firefox (web browser)' 'vscodium-bin (IDE)' 'thunar (file manager)' 'nemo (secondary file manager)' 'fuzzel (secondary app launcher)'
+install-optional-deps 'gammastep (night light)' 'wlogout (secondary session menu)' 'grimblast-git (screenshot freeze)' 'hyprpicker-git (colour picker)' 'foot (terminal emulator)' 'firefox (web browser)' 'vscodium-bin (IDE)' 'thunar (file manager)' 'nemo (secondary file manager)' 'fuzzel (secondary app launcher)' 'ydotool (alternate paste)'
 
 set -l hypr $CONFIG/../hypr
+set -l uwsm $CONFIG/../uwsm
 
 # Cause hyprland autogenerates a config file when it is removed
 confirm-overwrite $hypr dummy
 git clone 'https://github.com/caelestia-dots/hypr.git' /tmp/caelestia-hypr
 rm -rf $hypr && mv /tmp/caelestia-hypr $hypr
 
-pacman -Q ydotool &> /dev/null || sudo pacman -S --needed --noconfirm ydotool
-systemctl --user enable --now ydotool.service
+# Install uwsm envs
+confirm-overwrite $uwsm
+mv $hypr/uwsm $uwsm
 
-sleep .5
+# Enable ydotool if installed
+pacman -Q ydotool &> /dev/null && systemctl --user enable --now ydotool.service
+
+# Reload hyprland config to get rid of error messages
+sleep .1
 hyprctl reload
 
 log 'Done.'
