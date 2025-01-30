6119208935bb9f232b982cad5129e2bd60302a84
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-01-30 12:47:46 +1100

    install: hypr optional trash-cli

--- install/hypr.fish
+++ install/hypr.fish
@@ -3,7 +3,7 @@
 . (dirname (status filename))/util.fish
 
 install-deps git uwsm hyprland-git hyprpaper-git hyprlock-git hypridle-git polkit-gnome gnome-keyring wl-clipboard wireplumber
-install-optional-deps 'gammastep (night light)' 'wlogout (secondary session menu)' 'grimblast-git (screenshot freeze)' 'hyprpicker-git (colour picker)' 'foot (terminal emulator)' 'firefox (web browser)' 'vscodium-bin (IDE)' 'thunar (file manager)' 'nemo (secondary file manager)' 'fuzzel (secondary app launcher)' 'ydotool (alternate paste)'
+install-optional-deps 'gammastep (night light)' 'wlogout (secondary session menu)' 'grimblast-git (screenshot freeze)' 'hyprpicker-git (colour picker)' 'foot (terminal emulator)' 'firefox (web browser)' 'vscodium-bin (IDE)' 'thunar (file manager)' 'nemo (secondary file manager)' 'fuzzel (secondary app launcher)' 'ydotool (alternate paste)' 'trash-cli (auto trash)'
 
 set -l hypr $CONFIG/../hypr
 set -l uwsm $CONFIG/../uwsm
