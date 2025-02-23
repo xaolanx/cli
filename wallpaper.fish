9b1120219b933795a49010fde5561eb9f03d1a61
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-23 22:06:46 +1100

    wallpaper: hide scheme update output

--- wallpaper.fish
+++ wallpaper.fish
@@ -117,7 +117,7 @@ else
     set -q _flag_T && set -l theme --theme $_flag_T
     $src/scheme/gen-scheme.fish $theme $chosen_wallpaper > $src/data/schemes/dynamic.txt
     if test -f $C_STATE/scheme/current.txt -a "$(cat $C_STATE/scheme/current-name.txt)" = 'dynamic'
-        caelestia scheme dynamic
+        caelestia scheme dynamic > /dev/null
     end
 
     # Store the wallpaper chosen
