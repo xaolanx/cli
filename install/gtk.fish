781e762ff1dae21216d2bb3c1311e486e0f4f0c5
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-02-24 11:27:10 +1100

    install: gtk set theme

--- install/gtk.fish
+++ install/gtk.fish
@@ -3,6 +3,7 @@
 . (dirname (status filename))/util.fish
 
 install-deps git adw-gtk-theme
+install-optional-deps 'papirus-icon-theme (icon theme)'
 
 set -l dist $C_DATA/gtk
 
@@ -11,5 +12,12 @@ update-repo gtk $dist
 
 # Install systemd service
 setup-systemd-monitor gtk $dist
+
+# Set theme
+gsettings set org.gnome.desktop.interface gtk-theme \'adw-gtk3-dark\'
+if pacman -Q papirus-icon-theme &> /dev/null
+    read -l -p "Set icon theme to Papirus-Dark? [Y/n] ' -n" confirm
+    test "$confirm" = 'n' -o "$confirm" = 'N' || gsettings set org.gnome.desktop.interface icon-theme \'Papirus-Dark\'
+end
 
 log 'Done.'
