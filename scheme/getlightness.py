5955977cace292f4ef432d5601d2f3acd0858bbe
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-07 23:13:25 +1100

    install: add materialyoucolor dep

--- scheme/getlightness.py
+++
@@ -1,8 +0,0 @@
-#!/bin/python3
-
-import sys
-from colorsys import rgb_to_hls
-
-if __name__ == "__main__":
-    for arg in sys.argv[1:]:
-        print(rgb_to_hls(*tuple(int(arg[i:i+2], 16) for i in (0, 2, 4)))[1])
