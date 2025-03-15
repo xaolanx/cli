8543cb327f5806725f3127a3ac295091fa2b70de
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-03-15 15:32:21 +1100

    pickers: add placeholders
    Add placeholders for clipboard, clipboard delete and emoji picker
    Also better prompt for clipboard delete

--- clipboard-delete.fish
+++ clipboard-delete.fish
@@ -1,4 +1,4 @@
 #!/bin/fish
 
-set -l chosen_item (cliphist list | fuzzel --dmenu --prompt='Delete from clipboard: ')
+set -l chosen_item (cliphist list | fuzzel --dmenu --prompt='del > ' --placeholder='Delete from clipboard')
 test -n "$chosen_item" && echo "$chosen_item" | cliphist delete
