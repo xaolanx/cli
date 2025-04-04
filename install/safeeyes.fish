82d538a6ad96d6a7dc821a879e0597426758a7bf
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-04-04 18:01:39 +1100

    install: use systemd services for shell and safeeyes

--- install/safeeyes.fish
+++ install/safeeyes.fish
@@ -7,4 +7,28 @@ install-deps git dart-sass aylurs-gtk-shell-git alsa-utils libappindicator-gtk3
 # Update/Clone repo
 update-repo safeeyes $C_DATA/safeeyes
 
+if which systemctl &> /dev/null
+    log 'Installing systemd service...'
+
+    set -l systemd $CONFIG/systemd/user
+    mkdir -p $systemd
+    echo -n "
+[Unit]
+Description=Protect your eyes from eye strain using this simple and beautiful, yet extensible break reminder.
+After=graphical-session.target
+
+[Service]
+Type=exec
+ExecStart=/usr/bin/ags run -d $C_DATA/safeeyes
+Restart=on-failure
+Slice=app-graphical.slice
+
+[Install]
+WantedBy=graphical-session.target
+" > $systemd/caelestia-safeeyes.service
+
+    systemctl --user daemon-reload
+    systemctl --user enable --now caelestia-safeeyes.service
+end
+
 log 'Done.'
--- install/shell.fish
+++ install/shell.fish
@@ -13,4 +13,28 @@ update-repo shell $shell
 cd $shell || exit
 npm install
 
+if which systemctl &> /dev/null
+    log 'Installing systemd service...'
+
+    set -l systemd $CONFIG/systemd/user
+    mkdir -p $systemd
+    echo -n "
+[Unit]
+Description=A visually stunning and feature-rich desktop shell made for the Caelestia project.
+After=graphical-session.target
+
+[Service]
+Type=exec
+ExecStart=$shell/run.fish
+Restart=on-failure
+Slice=app-graphical.slice
+
+[Install]
+WantedBy=graphical-session.target
+" > $systemd/caelestia-shell.service
+
+    systemctl --user daemon-reload
+    systemctl --user enable --now caelestia-shell.service
+end
+
 log 'Done.'
