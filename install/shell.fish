0d7dd0707a7c0e48f228b380bbdc0ff871083d2a
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-06-08 00:44:20 +1000

    install: shell install add lm_sensors

--- install/shell.fish
+++ install/shell.fish
@@ -2,6 +2,11 @@
 
 . (dirname (status filename))/util.fish
 
+if ! pacman -Q lm_sensors > /dev/null
+    sudo pacman -S --noconfirm lm_sensors
+    sudo sensors-detect --auto
+end
+
 install-deps git quickshell curl jq ttf-material-symbols-variable-git ttf-jetbrains-mono-nerd ttf-ibm-plex app2unit-git fd fish python-aubio python-pyaudio python-numpy cava networkmanager bluez-utils ddcutil brightnessctl
 install-optional-deps 'uwsm (for systems using uwsm)'
 
