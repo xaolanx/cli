6543c9505befc2129fe6bec66b2e7829a5be6b9e
2 * r + 2 * t <61896496+soramanew@users.noreply.github.com>
2025-06-11 00:48:49 +1000

    parser: fix error when no args

--- src/caelestia/__init__.py
+++ src/caelestia/__init__.py
@@ -3,7 +3,11 @@ from caelestia.parser import parse_args
 
 def main() -> None:
     args = parse_args()
-    args.cls(args).run()
+    if "cls" in args:
+        args.cls(args).run()
+    else:
+        import sys
+        print("No arguments given", file=sys.stderr)
 
 if __name__ == "__main__":
     main()
