ca93616da717f865cf708d1cf26d6964e4231d90
Soramane <61896496+soramanew@users.noreply.github.com>
2025-07-15 17:16:53 +1000

    feat: add nix flake

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "CLI for Caelestia dots";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+    app2unit = {
+      url = "github:soramanew/app2unit";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  } @ inputs: let
+    forAllSystems = fn:
+      nixpkgs.lib.genAttrs nixpkgs.lib.platforms.linux (
+        system: fn nixpkgs.legacyPackages.${system}
+      );
+  in {
+    formatter = forAllSystems (pkgs: pkgs.alejandra);
+
+    packages = forAllSystems (pkgs: rec {
+      caelestia-cli = pkgs.callPackage ./default.nix {
+        rev = self.rev or self.dirtyRev;
+        app2unit = inputs.app2unit.packages.${pkgs.system}.default;
+      };
+      default = caelestia-cli;
+    });
+
+    # devShells = forAllSystems (pkgs: {
+    #   default = pkgs.mkShellNoCC {
+    #     inputsFrom = [self.packages.${pkgs.system}.caelestia-cli];
+    #     packages = [inputs.caelestia-shell.packages.${pkgs.system}.default];
+    #   };
+    # });
+  };
+}
